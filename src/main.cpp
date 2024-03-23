10cf179820a47f72b70d48f6de129713ede05d12
José Ángel López <jose.a.l.e@hotmail.com>
2024-03-23 17:09:50 +0100

    feature: initial perceptron main loop (WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,38 @@
 #include <vector>
 
 int main() {
-	// TODO
+	// Initializate Perceptron class
+	// Perceptron perceptron(); TODO
+
+	// Inputs
+	std::vector<std::vector<double>> inputs = {
+		{0, 0},
+		{0, 1},
+		{1, 0},
+		{1, 1}
+	};
+	
+	// AND function labels
+	std::vector<int> labels = { 0, 1, 1, 1 };
+
+	// Training of the perceptron
+	int epocs;
+	std::cout << "Number of epocs: ";
+	std::cin >> epocs;
+	for (int i = 0; i < epocs; i++) {
+		std::cout << "Epoch: " << i << std::endl;
+		for (size_t j = 0; j < inputs.size(); j++) {
+			// perceptron.train(inputs[j], labels[j]); TODO
+			std::cout << "\t - Training process for input: " << inputs[j][0] << " label: " << labels[j] << std::endl;
+			// more information...
+		}
+	}
+
+	// Output of the trained perceptron
+	std::cout << "Input values: " << inputs[0][0] << ", " << inputs[0][1] << std::endl;
+	std::cout << "Label value: 0" << std::endl;
+	// std::cout << "Predicted output: " << perceptron.predict(inputs[0]); TODO
+	std::cout << "Predicted output: 0";
+
 	return 0;
 }
--- src/perceptron.cpp
+++ src/perceptron.cpp
@@ -1,4 +1,4 @@
 #include <iostream>
 #include <vector>
 
-// TODO
+// TODO class implementation
